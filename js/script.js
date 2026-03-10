255b2c7f1df6c37659fac61e15a102183f83e4cf
Pablo <phenriquesy@gmail.com>
2026-03-10 00:00:44 -0300

    fazendo algumas mudanças no hero

--- js/script.js
+++ js/script.js
@@ -13,3 +13,45 @@ const observer = new IntersectionObserver(
 );
 
 reveals.forEach((element) => observer.observe(element));
+
+const imagens = [
+"/img/img-1-hero.webp",
+"/img/imagem de serviço1.webp",
+"/img/imagem-limpeza.webp"
+]
+
+let indice = 0
+
+const hero = document.querySelector(".hero")
+const backgrounds = document.querySelectorAll(".hero-bg")
+
+backgrounds[0].style.backgroundImage = `url('${imagens[0]}')`
+
+let ativo = 0
+
+function trocarImagem(dir){
+
+    if(dir === "right"){
+        indice = (indice + 1) % imagens.length
+    } else {
+        indice = (indice - 1 + imagens.length) % imagens.length
+    }
+
+    const atual = backgrounds[ativo]
+    const proxima = backgrounds[ativo === 0 ? 1 : 0]
+
+    proxima.style.backgroundImage = `url('${encodeURI(imagens[indice])}')`
+
+    proxima.style.opacity = 1
+    atual.style.opacity = 0
+
+    ativo = ativo === 0 ? 1 : 0
+}
+
+function btnRgth(){
+    trocarImagem("right")
+}
+
+function btnLft(){
+    trocarImagem("left")
+}
